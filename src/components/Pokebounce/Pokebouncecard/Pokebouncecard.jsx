d945fc811ee7a2800c1e875ce605af49bf808e55
Taniuop <taniuop@gmail.com>
2021-12-20 23:33:18 +0100

    icon and name fix

--- src/components/Pokebounce/Pokebouncecard/Pokebouncecard.jsx
+++ src/components/Pokebounce/Pokebouncecard/Pokebouncecard.jsx
@@ -4,9 +4,9 @@ const Pokebouncecard = (props) => {
   return <div className="pokeContainer">
     <div className="pokecards">
       <h2>{props.pokemonInfo.name.toUpperCase()}</h2>
-      <h3>ID: {props.pokemonInfo.id}</h3>
       <h3>Weight: {props.pokemonInfo.weight}</h3>
-      <img src={props.pokemonInfo.img} alt="" />
+      <img className="pokeimg" src={props.pokemonInfo.img} alt="" />
+      <h3>ID: {props.pokemonInfo.id}</h3>
     </div>
   </div>;
 };
